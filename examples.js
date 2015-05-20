7b86229624a48a7024c421038dd583e220fec7b8
Josh Balfour <josh@joshbalfour.co.uk>
2015-05-20 22:06:55 +0100

    Commentz

--- examples.js
+++ examples.js
@@ -1,9 +1,14 @@
 
 var app = require('./app.js');
 
+// gotta be a number
 var howSoonIsSoonInMinutes = 30;
+// CRS code (read: http://www.nationalrail.co.uk/stations_destinations/48541.aspx)
 var atStation = 'CBW';
 
+/*
+	Trains is an array of objects
+ */
 app.getTrainsArrivingOrLeavingSoon(atStation,howSoonIsSoonInMinutes,function(error, trains){
 	if (error){
 		console.log('Errored:',error);
