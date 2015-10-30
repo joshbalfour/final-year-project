74ddb31e1ecf4a9aaddb0741006d07210c68fedb
Kieran Jones <kjones1876@gmail.com>
2015-10-30 00:33:46 +0000

    Removed the unneeded stuff from laravel. Added in a default CrossingsController

---
+++ Application/app/Http/Controllers/CrossingsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class CrossingsController extends Controller
+{
+
+    /**
+     * Returns all of the current crossing with
+     * a small piece of data and their current up/down status
+     * 
+     * @return Array
+     */
+    public function index()
+    {
+        return [
+            // Data to go here
+            // Make DB calls happen.
+        ];
+    }
+}
