0fd829f89a75764d45531d7f411609f7d0df618f
Josh Balfour <josh@joshbalfour.co.uk>
2015-11-24 23:09:08 +0000

    CSV Import crossings to table crossing

--- Application/app/Console/Commands/ImportCrossings.php
+++ Application/app/Console/Commands/ImportCrossings.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class ImportCrossings extends Command
 {
@@ -37,15 +38,32 @@ class ImportCrossings extends Command
      */
     public function handle()
     {
-        //
         $csvString = file_get_contents('./database/data/Level Crossing Data.csv');
-        $csv = str_getcsv($csvString);
+        
+        // getCSV does not do what you think it does!!
+
+        $lines = explode(PHP_EOL, $csvString);
+        $csv = array();
+        foreach ($lines as $line) {
+            $csv[] = str_getcsv($line);
+        }
+
         array_shift($csv);
         $toInsert = [];
         foreach ($csv as $row) {
+            
             $toInsert[] = [
-                'id' => $row[0]
+                'id' => $row[0],
+                'crossing_name' => $row[1],
+                'crossing_type' => $row[2],
+                'loc' => DB::raw('GeomFromText("point('.$row[3].' '.$row[4].')")'),
+                'postcode' => $row[5],
+                'city' => $row[6],
+                'types_of_trains' => $row[11],
+                'line_speed' => $row[12],
+                'no_of_trains' => $row[13]
             ];
+            
         }
         DB::table('crossings')->insert($toInsert);
     }
