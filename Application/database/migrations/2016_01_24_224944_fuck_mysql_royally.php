c19c71e2042c7e15d89c92545d7e24a6844e9f88
Josh Balfour <josh@joshbalfour.co.uk>
2016-01-24 23:16:26 +0000

    add indexes

--- Application/database/migrations/2016_01_24_224944_fuck_mysql_royally.php
+++ Application/database/migrations/2016_01_24_224944_fuck_mysql_royally.php
@@ -17,8 +17,13 @@ class FuckMysqlRoyally extends Migration
         DB::statement("create index idx_train_times_rid on train_times (rid)");
 
         DB::statement(file_get_contents("/src/database/data/flat_train_times_with_crs.sql"));
-        
+
         DB::statement("create table train_times_with_crs as select * from v_train_times_with_crs");
+
+        DB::statement("create index crs on train_times_with_crs(from_crs, to_crs);");
+        DB::statement("create index from_time on train_times_with_crs(from_time);");
+        DB::statement("create index to_time on train_times_with_crs(to_time);");
+        
     }
 
     /**
@@ -33,5 +38,9 @@ class FuckMysqlRoyally extends Migration
         Schema::drop('v_train_times_with_crs');
         DB::statement("rename table x_train_times_with_crs to train_times_with_crs");
         DB::statement("alter table train_times drop index idx_train_times_rid");
+
+        DB::statement("alter table train_times_with_crs drop index crs");
+        DB::statement("alter table train_times_with_crs drop index from_time");
+        DB::statement("alter table train_times_with_crs drop index to_time");
     }
 }
